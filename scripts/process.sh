85031c9b3a2407ce8cd31e374b58a6103dbeed59
rjhale1971 <rjhale@northrivergeographic.com>
2025-06-05 09:07:10 -0400

    fixing data issue

--- scripts/process.sh
+++ scripts/process.sh
@@ -3,6 +3,7 @@
 [ -f ../data/bicycle_ways.overpassql ] && rm ../data/bicycle_ways.overpassql
 [ -f ../data/bicycle_routes.geojson ] && rm ../data/bicycle_routes.geojson
 [ -f ../data/bicycle_final.geojson ] && rm ../data/bicycle_final.geojson
+[ -f ../data/bicycle_ways.geojson ] && rm ../data/bicycle_ways.geojson
 
 
 #grab the data 
@@ -10,6 +11,6 @@ wget -O ../data/bicycle_ways.overpassql --post-file=bicycle_request.overpassql "
 
 ogr2ogr -f "GEOJSON" --config OSM_USE_CUSTOM_INDEXING NO ../data/bicycle_routes.geojson ../data/bicycle_ways.overpassql lines 
 
-ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/bicycle_final.geojson ../data/bicycle_ways.geojson
+ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/bicycle_final.geojson ../data/bicycle_routes.geojson
 
 
