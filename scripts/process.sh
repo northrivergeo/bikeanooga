ce379425d2987420e661278b73a6587486f0d0a9
northrivergeo <rjhale@northrivergeographic.com>
2025-07-07 22:06:22 -0400

    adding

--- scripts/process.sh
+++ scripts/process.sh
@@ -16,19 +16,27 @@
 [ -f ../data/schick_final.geojson ] && rm ../data/schick_final.geojson
 [ -f ../data/schick_ways.geojson ] && rm ../data/schick_ways.geojson
 
+[ -f ../data/bike_rentals.overpassql ] && rm ../data/bike_rentals.overpassql
+[ -f ../data/bike_rentals_points.geojson ] && rm ../data/bike_rentals_points.geojson
+[ -f ../data/bike_rentals_final.geojson ] && rm ../data/bike_rental_final.geojson
+[ -f ../data/bike_rentals.geojson ] && rm ../data/bike_rentals.geojson
+
 #grab the data 
 wget -O ../data/bicycle_ways.overpassql --post-file=bicycle_request.overpassql "https://overpass-api.de/api/interpreter"
 wget -O ../data/schick_ways.overpassql --post-file=schick_greenway_request.overpassql "https://overpass-api.de/api/interpreter"
 wget -O ../data/riverwalk_ways.overpassql --post-file=riverwalk_request.overpassql "https://overpass-api.de/api/interpreter"
+wget -O ../data/bike_rentals.overpassql --post-file=bike_rentals_request.overpassql "https://overpass-api.de/api/interpreter"
 
 #process the data 
 ogr2ogr -f "GEOJSON" --config OSM_USE_CUSTOM_INDEXING NO ../data/bicycle_routes.geojson ../data/bicycle_ways.overpassql lines 
 ogr2ogr -f "GEOJSON" --config OSM_USE_CUSTOM_INDEXING NO ../data/schick_routes.geojson ../data/schick_ways.overpassql lines 
 ogr2ogr -f "GEOJSON" --config OSM_USE_CUSTOM_INDEXING NO ../data/riverwalk_routes.geojson ../data/riverwalk_ways.overpassql lines 
+ogr2ogr -f "GEOJSON" --config OSM_USE_CUSTOM_INDEXING NO ../data/bike_rentals_points.geojson ../data/bike_rentals.overpassql lines 
 
 #clip it at the county line
 ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/bicycle_final.geojson ../data/bicycle_routes.geojson
 ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/riverwalk_final.geojson ../data/riverwalk_routes.geojson
 ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/schick_final.geojson ../data/schick_routes.geojson
+ogr2ogr -clipsrc ../data/hamilton_county.geojson ../data/bike_rentals_final.geojson ../data/bike_rentals_points.geojson
 
 
